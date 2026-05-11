599e191028377fc7567c70f7b296007dd89472f5
Yaluuuu <yaluc@uw.edu>
2026-05-11 15:14:54 -0700

    define variables

--- sketches/sketch15-hwk5.js
+++ sketches/sketch15-hwk5.js
@@ -2,12 +2,36 @@
 registerSketch('sk15', function (p) {
   const CANVAS_SIZE = 800;
 
+  // Define variables ---
+  let table;
+  let categories = [
+    "Time spent alone, by age of respondent (United States)",
+    "Time spent with partner, by age of respondent (United States)",
+    "Time spent with children, by age of respondent (United States)",
+    "Time spent with with parents, siblings and other family, by age of respondent (United States)",
+    "Time spent with friends, by age of respondent (United States)",
+    "Time spent with coworkers, by age of respondent (United States)"
+  ];
+  let colors = ['#2a363b', '#e84a5f', '#ff847c', '#fecea8', '#99b898', '#355c7d'];
+
+  p.preload = function () {
+    table = p.loadTable('/Users/chenyalu/Desktop/IMT561/week6/homework5/Americans_Time_spent_with_relationships_by_age.csv', 'csv', 'header');
+  };
+
   p.setup = function () {
     p.createCanvas(CANVAS_SIZE, CANVAS_SIZE);
+    p.noLoop();
   };
 
   p.draw = function () {
     p.background(255);
+
+    let margin = 60;
+    let w = p.width - margin * 2;
+    let h = p.height - margin * 2;
+    
+    if (!table) return; // security check for data loading
+
     p.noStroke();
     p.fill(70);
     p.textSize(32);
